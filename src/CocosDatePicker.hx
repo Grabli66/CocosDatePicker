ccdc979532aee98b6afa6b50fb28f9cbbec506f1
Grabli66 <grabli66@gmail.com>
2019-02-01 21:54:30 +0300

    Month days

--- src/CocosDatePicker.hx
+++ src/CocosDatePicker.hx
@@ -1,149 +1,250 @@
+import culture.CultureData;
 import DateManager.DayInfo;
 import culture.CultureManager;
 import js.Browser.*;
 import js.html.InputElement;
 import js.html.Element;
 
-// Refference: 
+// Refference:
 // http://t1m0n.name/air-datepicker/docs/index-ru.html
 // https://www.cssscript.com/demo/minimal-inline-calendar-date-picker-vanilla-javascript/
+
+/**
+ * Picker day info for display
+ */
+typedef PickerDayInfoImpl = {
+	> DayInfo,
+
+	/**
+	 * Foreign month
+	 */
+	var foreign:Bool;
+}
+
+@:forward
+abstract PickerDayInfo(PickerDayInfoImpl) {
+	/**
+	 * Constructor
+	 */
+	public function new(value:PickerDayInfoImpl) {
+		this = value;
+	}
+
+	@:from public static function from(value:DayInfo) {
+		return new PickerDayInfo({
+			monthDay: value.monthDay,
+			weekDay: value.weekDay,
+			month: value.month,
+			year: value.year,
+			foreign: false
+		});
+	}
+}
 
 /**
  * DatePicker widget for coconut.ui
  */
 @:less("cocos_date_picker.less")
 class CocosDatePicker extends coconut.ui.View {
-    /**
-     * Picker panel
-     */
-    @:ref var picker:Element;
+	/**
+	 * Picker panel
+	 */
+	@:ref var picker:Element;
+
+	/**
+	 * Date input element
+	 */
+	@:ref var input:InputElement;
+
+	/**
+	 * Start date
+	 */
+	@:attribute public var startDate:Date = @byDefault Date.now();
+
+	/**
+	 * Culture for localization
+	 */
+	@:attribute public var culture:String = @byDefault "en-GB";
+
+	/**
+	 * Current date
+	 */
+	@:state var currentDate:Date = Date.now();
 
     /**
-     * Date input element
+     * Selected day index
      */
-    @:ref var input:InputElement;
+    @:state var selectedDayIndex:Int = @byDefault 0;
 
     /**
-     * Start date
+     * Days in calendar
      */
-    @:attribute public var startDate:Date = @byDefault Date.now();
+    var pickDays:Array<PickerDayInfo>;
 
-    /**
-     * Culture for localization
-     */
-    @:attribute public var culture:String = @byDefault "en-GB";
+	/**
+	 * Add svg to buttons
+	 * TODO: remove and add SVG inline
+	 */
+	function addSvg() {
+		var element = document.querySelector(".nav.left");
+		element.innerHTML = AssetHelper.inlineText('./src/assets/chevron-left.svg');
+		element = document.querySelector(".nav.right");
+		element.innerHTML = AssetHelper.inlineText('./src/assets/chevron-right.svg');
+	}
 
-    /**
-     * Current date
-     */
-    @:state var currentDate:Date = Date.now();
+	/**
+	 * On component mount
+	 */
+	function viewDidMount() {
+		addSvg();
+	}
 
-    /**
-     * On component mount
-     */
-    function viewDidMount() {
-        var element = document.querySelector(".nav.left");
-        element.innerHTML = AssetHelper.inlineText('./src/assets/chevron-left.svg');
-        element = document.querySelector(".nav.right");
-        element.innerHTML = AssetHelper.inlineText('./src/assets/chevron-right.svg');
-    }
+	/**
+	 * On component update
+	 */
+	function viewDidUpdate() {
+		addSvg();
+	}
 
-    /**
-     * On input click handler
-     */
-    function onclick(e:js.html.Event) {                
-        showPicker();
-        registerBodyClick();
-    }
+	/**
+	 * On input click handler
+	 */
+	function onclick(e:js.html.Event) {
+		showPicker();
+		registerBodyClick();
+	}
 
-    /**
-     * On click out of picker     
-     */
-    function onOuterClick(e:js.html.Event) {        
-        if (picker.current != e.target && input.current != e.target) {
-            unregisterBodyClick();
-            hidePicker();
-        }
-    }
+	/**
+	 * On click out of picker
+	 */
+	function onOuterClick(e:js.html.Event) {
+		if (picker.current != e.target && input.current != e.target) {
+			unregisterBodyClick();
+			hidePicker();
+		}
+	}
 
-    /**
-     * Register handlers for click in document
-     */
-    function registerBodyClick() {
-        //document.addEventListener("click", onOuterClick);
-    }
+	/**
+	 * Register handlers for click in document
+	 */
+	function registerBodyClick() {
+		// document.addEventListener("click", onOuterClick);
+	}
 
-    /**
-     * Unregister handlers for click
-     */
-    function unregisterBodyClick() {
-       // document.removeEventListener("click", onOuterClick);
-    }
+	/**
+	 * Unregister handlers for click
+	 */
+	function unregisterBodyClick() {
+		// document.removeEventListener("click", onOuterClick);
+	}
 
-    /**
-     * Show picker
-     */
-    function showPicker() {        
-        picker.current.style.display = "block";
-        // TODO calculate real height
-        var inputHeight = input.current.clientHeight + 3;
-        picker.current.style.marginTop = '${inputHeight}px';        
-    }
+	/**
+	 * Show picker
+	 */
+	function showPicker() {
+		picker.current.style.display = "block";
+		// TODO calculate real height
+		var inputHeight = input.current.clientHeight + 3;
+		picker.current.style.marginTop = '${inputHeight}px';
+	}
 
-    /**
-     * Hide picker
-     */
-    function hidePicker() {
-        picker.current.style.display = "none";
-    }   
+	/**
+	 * Hide picker
+	 */
+	function hidePicker() {
+		picker.current.style.display = "none";
+	}
 
-    /**
-     * Handle day click
-     * @param day 
-     */
-    function onDayClick(day:DayInfo) {
-        trace(day);
-    }
+	/**
+	 * Handle day click
+	 * @param day
+	 */
+	function onDayClick(day:PickerDayInfo, idx:Int) {
+        selectedDayIndex = idx;
+	}
 
-    /**
-     * Handle left navigating button click
-     */
-    function onLeftClick() {
-        var year = currentDate.getFullYear();
+	/**
+	 * Handle left navigating button click
+	 */
+	function onLeftClick() {
+		var year = currentDate.getFullYear();
+		var month = currentDate.getMonth();
+
+		currentDate = new Date(year, month - 1, 1, 0, 0, 0);
+		trace(currentDate);
+	}
+
+	/**
+	 * Handle title click of calendar
+	 */
+	function onTitleClick() {}
+
+	/**
+	 * Handle right navigating button click
+	 */
+	function onRightClick() {
+		var year = currentDate.getFullYear();
+		var month = currentDate.getMonth();
+
+		currentDate = new Date(year, month + 1, 1, 0, 0, 0);
+		trace(currentDate);
+	}
+
+	/**
+	 * Return days for calendar
+	 */
+	function getDays(cult:CultureData):Array<PickerDayInfo> {
+		var pickDays = new Array<PickerDayInfo>();
         var month = currentDate.getMonth();
+		var year = currentDate.getFullYear();
 
-        currentDate = new Date(year, month - 1, 1, 0, 0, 0);
-        trace(currentDate);
-    }
+		var days = DateManager.instance.getMonthDays(month, year);
+		var firstDay:PickerDayInfo = days[0];
+		if (firstDay.weekDay > 0) {
+			var lastDate = new Date(year, month - 1, 1, 0, 0, 0);
+			var lastMonth = lastDate.getMonth();
+			var lastYear = lastDate.getFullYear();
+			var lastDays = DateManager.instance.getMonthDays(lastMonth, lastYear);
 
-    /**
-     * Handle title click of calendar
-     */
-    function onTitleClick() {
+			var idx = lastDays.length - firstDay.weekDay - 1;
+			for (i in 1...firstDay.weekDay + 1) {
+				var pickDay:PickerDayInfo = lastDays[idx + i];
+				pickDay.foreign = true;
+				pickDays.push(pickDay);
+			}
+		}
 
-    }
+		for (day in days)
+			pickDays.push(day);
 
-    /**
-     * Handle right navigating button click
-     */
-    function onRightClick() {        
-        var year = currentDate.getFullYear();
-        var month = currentDate.getMonth();
+		var lastDay:PickerDayInfo = days[days.length - 1];
+		if (lastDay.weekDay < DateManager.MAX_WEEK_OF_DAY) {
+			var len = DateManager.MAX_WEEK_OF_DAY - lastDay.weekDay;
+			var nextDate = new Date(year, month + 1, 1, 0, 0, 0);
+			var nextMonth = nextDate.getMonth();
+			var nextYear = nextDate.getFullYear();
+			var nextDays = DateManager.instance.getMonthDays(nextMonth, nextYear);
+			for (i in 0...len) {
+				var pickDay:PickerDayInfo = nextDays[i];
+                pickDay.foreign = true;
+				pickDays.push(pickDay);
+			}
+		}
 
-        currentDate = new Date(year, month + 1, 1, 0, 0, 0);
-        trace(currentDate);
-    }
+		return pickDays;
+	}
 
 	/**
 	 * Render view
 	 */
 	function render() {
-        var cult = CultureManager.instance.getCulture(culture);
-        var weekdays = cult.nameDaysShortest;
-        var month = currentDate.getMonth();
-        var year = currentDate.getFullYear();
-        var monthName = cult.nameMonths[month];
-        var days = DateManager.instance.getMonthDays(month, year);
+		var cult = CultureManager.instance.getCulture(culture);
+		var month = currentDate.getMonth();
+		var year = currentDate.getFullYear();
+		var monthName = cult.nameMonths[month];
+
+		pickDays = getDays(cult);
+		var weekdays = cult.nameDaysShortest;
 
 		return hxx('
             <div class="date-picker">
@@ -161,13 +262,24 @@ class CocosDatePicker extends coconut.ui.View {
                             </for>
                         </div>
                         <div class="month-days">
-                            <for ${day in days}>
-                                <div class="day" onclick=${onDayClick(day)}>${day.day}</div>
+                            <for ${i in 0...pickDays.length}>
+                                <let day=${pickDays[i]}>
+                                    <if ${day.foreign}>
+                                        <div class="day foreign" onclick=${onDayClick(day, i)}>${day.monthDay}</div>
+                                    <else>
+                                        <if ${selectedDayIndex == i}>
+                                            <div class="day selected">${day.monthDay}</div>
+                                        <else>
+                                            <div class="day" onclick=${onDayClick(day, i)}>${day.monthDay}</div>
+                                        </if>
+                                    </if>
+                                </let>
                             </for>
                         </div>
                     </div>
                 </div>
             </div>
         ');
+
 	}
 }
--- src/DateManager.hx
+++ src/DateManager.hx
@@ -2,8 +2,24 @@
  * Information about day
  */
 typedef DayInfo = {
-	var day:Int;
+	/**
+	 * 1..31
+	 */
+	var monthDay:Int;
+
+	/**
+	 * 0..6
+	 */
+	var weekDay:Int;
+
+	/**
+	 * 0..12
+	 */
 	var month:Int;
+
+	/**
+	 * 0..infinite
+	 */
 	var year:Int;
 }
 
@@ -11,6 +27,11 @@ typedef DayInfo = {
  * For getting dates, months, years
  */
 class DateManager {
+	/**
+	 * Max week day number
+	 */
+	public static inline final MAX_WEEK_OF_DAY = 6; // 0 - Sunday
+
 	/**
 	 * Instance of manager
 	 */
@@ -23,12 +44,26 @@ class DateManager {
 
 	/**
 	 * Return month days
-     * Month 0..11
+	 * Month 0..11
 	 */
 	public function getMonthDays(month:Int, year:Int):Array<DayInfo> {
 		var date = new Date(year, month, 1, 0, 0, 0);
 		var dayCount = DateTools.getMonthDays(date);
-		var res = [for (day in 1...dayCount + 1) {day: day, month: month, year: year}];
+		var weekDayOfFirst = date.getDay();
+
+		var res = new Array<DayInfo>();
+		for (day in 1...dayCount + 1) {
+			res.push({
+				monthDay: day,
+				weekDay: weekDayOfFirst,
+				month: month,
+				year: year
+			});
+
+			weekDayOfFirst += 1;
+			if (weekDayOfFirst > MAX_WEEK_OF_DAY)
+				weekDayOfFirst = 0;
+		}
 		return res;
 	}
 }
